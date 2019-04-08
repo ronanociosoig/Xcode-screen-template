817f5276d485e43379532edafdc8292ec62d0df7
Ronan O Ciosoig <ronanoc@icloud.com>
2019-04-08 19:45:53 +0200

    Update install script.

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Install the Meranti MVP file template into the user library for Xcode."
+USER_TEMPLATE_DIR=~/Library/Developer/Xcode/Templates/File\ Templates/Meranti\ MVP\ Module.xctemplate
+TEMPLATE_DIR="Meranti MVP Module.xctemplate"
 
+if [ -d "$USER_TEMPLATE_DIR" ]; then
+rm -r "$USER_TEMPLATE_DIR"
+  echo "Removed existing template."
+fi
 
+echo "Copy over new template."
+mv "$TEMPLATE_DIR" "$USER_TEMPLATE_DIR"
